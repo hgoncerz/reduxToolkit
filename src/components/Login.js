5d5ea77112b93525506c9c59efe6919cbdd2e3dd
hgoncerz <hubertg489655@gmail.com>
2022-06-21 17:35:20 +0200

    complete

---
+++ src/components/Login.js
@@ -0,0 +1,21 @@
+import React from "react";
+import { useDispatch } from "react-redux/es/exports";
+import { login, logout } from "../features/user";
+
+const Login = () => {
+  const dispatch = useDispatch();
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "Pedro", age: 20, email: "pedro@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
+      <button onClick={() => dispatch(logout())}>Logout</button>
+    </div>
+  );
+};
+
+export default Login;
